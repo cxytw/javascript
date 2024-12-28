d6d4f36aa891e27d8abb8a5202e8398fa1e1f4eb
陳心瑩 <cxytw77@gmail.com>
2024-12-28 11:06:55 +0800

    Add first test file.

---
+++ test.js
@@ -0,0 +1,15 @@
+let x = 2;
+let y = 10;
+let z = x + y;
+y = 100;
+console.log(z);
+console.log(y);
+console.log("end off first section");
+
+let a = 11;
+a = a * 5; // assign
+console.log(a);
+
+// "John" + " "  + "Doe" // concatenate 把＿串在一起
+// Identifiers 標示符號
+// case sensitive 大小寫敏感（區分）
